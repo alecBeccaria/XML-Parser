80617be21b2070cec016634cb6cb70865bf4d7f5
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-10-17 15:51:08 -0600

    Update copyxml.py

--- copyxml.py
+++ copyxml.py
@@ -8,19 +8,22 @@ import pyodbc as db
 customer_list = []
 order_list = []
 line_list = []
+namespaces = {'x': 'http://schemas.datacontract.org/2004/07/DataGenerator'}
+ImportCount = 0
+File = 'customers.xml'
+sFile = 'sample.xml'
 
-def xml_parser(xmlFile):
+
+
+def customerParser(xmlFile):
     global customer_list
-    global line_list
-    global order_list
-
+    global namespaces
+    global ImportCount
     with open(xmlFile) as obj:
-        print('Starting Parse')
+        print('Starting Customer Parse')
         xmlTree = etree.parse(obj)
         xmlRoot = xmlTree.getroot()
-
-    namespaces = {'x': 'http://schemas.datacontract.org/2004/07/DataGenerator'}
-    ImportCount = 0
+        obj.close()
 
     print('building lists')
     for customer in xmlRoot.findall('x:Customer', namespaces):
@@ -32,58 +35,90 @@ def xml_parser(xmlFile):
         cust_tuples = [v for k, v in customer_dict.items()]
         customer_list.append(cust_tuples)
 
-        for orders in customer.findall('x:Orders', namespaces):
-            for order in orders.findall('x:Order', namespaces):
-                order_details = {'OrderId': 'null', 'CustomerId': 'null', 'Total': 'null'}
-                order_details['OrderId'] = order.find('x:OrderId', namespaces).text
-                OrderId = order.find('x:OrderId', namespaces).text
-                order_details['CustomerId'] = order.find('x:CustomerId', namespaces).text
-                order_details['Total'] = order.find('x:Total', namespaces).text
-                order_tuples = [v for k, v in order_details.items()]
-                order_list.append(order_tuples)
-
-
-                for lines in order.findall('x:Lines', namespaces):
-                    for OrderLine in lines.findall('x:OrderLine', namespaces):
-                        order_line = {'OrderLineId': 'null', 'OrderId': 'null', 'Qty': 'null', 'Price': 'null',
-                                      'LineTotal': 'null', 'ProductId': 'null'}
-                        order_line['OrderLineId'] = OrderLine.find('x:OrderLineId', namespaces).text
-                        order_line['Qty'] = OrderLine.find('x:Qty', namespaces).text
-                        order_line['Price'] = OrderLine.find('x:Price', namespaces).text
-                        order_line['LineTotal'] = OrderLine.find('x:Total', namespaces).text
-                        order_line['ProductId'] = OrderLine.find('x:ProductId', namespaces).text
-                        order_line['OrderId'] = OrderId
-                        tuples = [v for k, v in order_line.items()]
-                        line_list.append(tuples)
-                        OrderLine.clear()
-                        del OrderLine, order_line, tuples
-                    lines.clear()
-                    del lines
-                order.clear()
-                del order, order_tuples
-            orders.clear()
-            del orders
-
-        if len(customer_list) > 1000:
+        if len(customer_list) > 10000:
             sql_customers(customer_list)
             customer_list.clear()
             ImportCount += 1
             print(ImportCount)
 
-        if len(order_list) > 1000:
+        customer.clear()
+        del customer_dict, cust_tuples
+    del xmlTree, xmlRoot
+
+
+def orderParser(xmlFile):
+    global namespaces
+    global order_list
+    global ImportCount
+    with open(xmlFile) as obj:
+        print('Starting Order Parse')
+        xmlTree = etree.parse(obj)
+        xmlRoot = xmlTree.getroot()
+        obj.close()
+
+    for customer in xmlRoot.findall('x:Customer', namespaces):
+        for orders in customer.findall('x:Orders', namespaces):
+            for order in orders.findall('x:Order', namespaces):
+                order_details = {'OrderId': 'null', 'CustomerId': 'null', 'Total': 'null'}
+                order_details['OrderId'] = order.find('x:OrderId', namespaces).text
+
+                order_details['CustomerId'] = order.find('x:CustomerId', namespaces).text
+                order_details['Total'] = order.find('x:Total', namespaces).text
+                order_tuples = [v for k, v in order_details.items()]
+                order_list.append(order_tuples)
+                order.clear()
+            orders.clear()
+        customer.clear()
+        if len(order_list) > 10000:
             sql_orders(order_list)
             order_list.clear()
             ImportCount += 1
             print(ImportCount)
+    del xmlTree, xmlRoot
 
-        if len(line_list) > 1000:
+
+
+def lineParser(xmlFile):
+    global line_list
+    global namespaces
+    global ImportCount
+    with open(xmlFile) as obj:
+        print('Starting Line Parse')
+        xmlTree = etree.parse(obj)
+        xmlRoot = xmlTree.getroot()
+        obj.close()
+
+    for customer in xmlRoot.findall('x:Customer', namespaces):
+        for orders in customer.findall('x:Orders', namespaces):
+            for order in orders.findall('x:Order', namespaces):
+                OrderId = order.find('x:OrderId', namespaces).text
+                for lines in order.findall('x:Lines', namespaces):
+                    for OrderLine in lines.findall('x:OrderLine', namespaces):
+                        order_line = {'OrderLineId': 'null', 'OrderId': 'null', 'Qty': 'null', 'Price': 'null', 'LineTotal': 'null', 'ProductId': 'null'}
+                        order_line['OrderLineId'] = OrderLine.find('x:OrderLineId', namespaces).text
+                        order_line['OrderId'] = OrderId
+                        order_line['Qty'] = OrderLine.find('x:Qty', namespaces).text
+                        order_line['Price'] = OrderLine.find('x:Price', namespaces).text
+                        order_line['LineTotal'] = OrderLine.find('x:Total', namespaces).text
+                        order_line['ProductId'] = OrderLine.find('x:ProductId', namespaces).text
+
+                        tuples = [v for k, v in order_line.items()]
+                        line_list.append(tuples)
+                        OrderLine.clear()
+                        del order_line, tuples
+                    lines.clear()
+                order.clear()
+            orders.clear()
+        customer.clear()
+        if len(line_list) > 10000:
             sql_orderlines(line_list)
             line_list.clear()
             ImportCount += 1
             print(ImportCount)
+    del xmlTree, xmlRoot
 
-        customer.clear()
-        del customer_dict, cust_tuples
+
+
 
 
 def sql_customers(customers):
@@ -137,7 +172,9 @@ def testConn():
 
 
 if __name__ == "__main__":
-    # testConn()
-    # xmlFile = open('customers.xml', 'r')
-    xml_parser('customers.xml')
-    # sql_dbms(customers)
+    customerParser(File)
+    ImportCount = 0
+    orderParser(File)
+    ImportCount = 0
+    lineParser(File)
+
